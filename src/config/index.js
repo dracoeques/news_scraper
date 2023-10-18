180d7ffd8935fb93a954243afa70c99f93f0ec77
dracoeques <draceques@gmail.com>
2023-10-18 18:42:31 +0400

    fix issue on config

--- src/config/index.js
+++ src/config/index.js
@@ -14,9 +14,11 @@ switch (ENVIRONMENT) {
   case "production":
     config.db = process.env.MONGO_PROD_URI;
     config.port = process.env.PROD_PORT;
+    break;
   case "dev":
     config.db = process.env.MONGO_DEV_URI;
     config.port = process.env.DEV_PORT;
+    break;
   default:
     config.db = process.env.MONGO_TEST_URI;
     config.port = process.env.TEST_PORT;
